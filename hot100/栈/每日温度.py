ebda1de3b7ccd7d3f2f758881b6315fb6b679a62
HanFaye <3294196260@qq.com>
2025-09-24 16:09:22 +0800

    add

--- hot100/栈/每日温度.py
+++ hot100/栈/每日温度.py
@@ -1,1 +1,19 @@
-## TODO：单调栈
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            while len(stack)>0 and stack[-1][1] < temperatures[i]:
+                idx, val = stack.pop()
+                res[idx] = i - idx 
+                
+            stack.append((i, temperatures[i]))
+                
+        return res 
+    
+if __name__=="__main__":
+    print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
+    
+    
