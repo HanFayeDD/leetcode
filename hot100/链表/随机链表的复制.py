8ad8c4d2197c1135ae026372154cd3050ab56658
HanFaye <3294196260@qq.com>
2025-09-25 20:15:23 +0800

    add

--- hot100/链表/随机链表的复制.py
+++ hot100/链表/随机链表的复制.py
@@ -1,1 +1,38 @@
-## TODO
+from typing import List, Optional, Dict
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next:Node = next
+        self.random:Node = random
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        old_new_node_dict:Dict[Node, Node] = dict()
+        cur = head
+        while cur is not None:
+            old_new_node_dict[cur] = Node(cur.val)
+            cur = cur.next
+            
+        old = head
+        while old is not None:
+            new = old_new_node_dict[old]
+            if old.next is None:
+                new.next = None
+            else:
+                new.next = old_new_node_dict[old.next]
+            if old.random is None:
+                new.random = None
+            else:
+                new.random = old_new_node_dict[old.random]
+            old = old.next
+            
+        return old_new_node_dict.get(head, None)
+
+if __name__=="__main__":
+    d = dict()
+    d[None] = 1
